f353bc06240e62e12bf75c23f57d8e1748fed235
Pradeep Shrestha <shresthapradeep666@gmail.com>
2025-01-12 16:48:26 +0545

    login form

--- resources/views/allposts.blade.php
+++ resources/views/allposts.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.css" />
+</head>
   <body>
+
     <div class="container">
         <div class="row">
             <div class=" bg-dark text-white mb-4">
@@ -76,6 +78,7 @@
       </div>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/2.2.1/js/dataTables.js"></script>
 
     <script>
         document.querySelector("#logoutBtn").addEventListener('click',function(){
